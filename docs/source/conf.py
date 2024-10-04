35cf92b4e6251a5a0c58b9a587558fea10cfa487
LUHMW <132049891+LUHMW@users.noreply.github.com>
2024-10-04 10:21:35 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = 'NFDI4ING_Wort-Bildmarke_POS_RGB.svg'  
+html_logo = 'NFDI4ING_Wort-Bildmarke_POS_RGB_200.png'  
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
